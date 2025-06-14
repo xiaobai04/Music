552c891ca65068bb322170d6012ab6d6862919af
小白 <89311712+xiaobai04@users.noreply.github.com>
2025-06-14 14:02:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import sys
-import logging
-
-sys.setrecursionlimit(10000)  # 临时调高避免 RecursionError
-logging.basicConfig(level=logging.DEBUG)
-
 # main.py
 from ui.tkinter_ui import PlayerApp
 import tkinter as tk
