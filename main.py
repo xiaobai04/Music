3ad3e17b88de140b4c2604bf04eb1a1c4a97653e
小白 <89311712+xiaobai04@users.noreply.github.com>
2025-06-21 18:01:34 +0200

    feat: enhance player ui

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # main.py
 from ui.tkinter_ui import PlayerApp
 import ttkbootstrap as ttkb
+from utils.settings import load_settings
 
 if __name__ == "__main__":
-    root = ttkb.Window(themename="flatly")
+    settings = load_settings()
+    theme = settings.get("theme", "flatly")
+    root = ttkb.Window(themename=theme)
     app = PlayerApp(root)
     root.mainloop()
